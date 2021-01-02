c93d8a866a2bb109863fa98e41f0b4f80865cb2f
Arash Zolfaghari <arashz@gmail.com>
2021-01-01 22:35:03 -0800

    Retrieve businesses from mongo

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const connectionString = 'mongodb+srv://ahmad:bademjoon@cluster0.sdj3z.mongodb.net/companyrating?retryWrites=true&w=majority'
+const MongoClient = require('mongodb').MongoClient
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('companyrating')
 
-app.get('/', function (req, res) {
+        app.get('/businesses', (req, res) => {
+            console.log('INSIDE slash')
+            db.collection('businesses')
+                .find({}).collation({ locale: "en" }).sort({name: 1})
+                .toArray()
+                .then(results => {
+                    //console.log("got result",results)
+                    res.send(results)
+                })
+                .catch(error => {console.error(error)
+                    console.log("got error",error)
+                    res.send({error})
+                })
+        })
+
+
+        app.listen(process.env.PORT || 8080);
+
+
+    })
+    .catch(error => console.error(error))
+
+
+
+/*app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+});*/
 
-app.listen(process.env.PORT || 8080);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import axios from 'axios';
+import './App.css';
 function App() {
+  let [responseData, setResponseData] = React.useState('');
+  const fetchData = React.useCallback(() => {
+    axios({
+      method: 'GET',
+      url: '/businesses',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+        'Content-Type': 'application/json',
+      },
+      params: {
+        language_code: 'en',
+      },
+    })
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const fetchBusiness = (businessId) =>{
+    console.log(businessId)
+  }
+  React.useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetching Data with React Hooks</h1>
+      {/*<button type="button" onClick={fetchData}>
+        Click for Data
+      </button>*/}
+        {responseData &&
+          responseData.map((x) => {
+            return <div key={x._id}
+                style={{
+                    backgroundColor: '#ece',
+                    padding: 5,
+                  margin:10, textAlign:'Left'
+
+                }} onClick={()=> fetchBusiness(x._id)}
+            >{x.name}
+
+
+            </div>;
+          })}
+
     </div>
   );
 }
-
 export default App;
